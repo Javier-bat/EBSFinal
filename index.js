039aff8d93d5d91c0bde6bd19224b39986dbba3f
juanjavito <juanjavito@gmail.com>
2022-02-21 07:22:58 -0300

    npm ng heroku

--- index.js
+++ index.js
@@ -399,7 +399,7 @@ app.get('/pedido/:id', (req, res) => {
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/frontend/build/index.html'))
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
 })
 
 
